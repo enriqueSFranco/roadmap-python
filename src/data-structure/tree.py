9ca25a076f93da9f2d2735199683e535cab7ad23
enriqueSFranco <enrique.sfranco04@gmail.com>
2024-01-10 17:56:04 -0600

    feat(data-structure): binary tree

---
+++ src/data-structure/tree.py
@@ -0,0 +1,15 @@
+class TreeNode:
+  def __init__(self, data) -> None:
+    self.data = data
+    self.left = None
+    self.right = None
+
+class BinaryTree:
+  def __init__(self, data) -> None:
+    self.root = TreeNode(data)
+    
+  def insertNode(self):
+    pass
+  
+  def insert(self):
+    pass
