f9d4e63da38ce6a5576ea6899e65e82f9b91e4a7
enriqueSFranco <enrique.sfranco04@gmail.com>
2024-09-04 13:44:15 -0600

    chore(): rename folders

--- src/dynamic_programming/fibonacci.py
+++ src/dynamic_programming/fibonacci.py
@@ -1,22 +1,3 @@
-def factorial_recursive(n: int) -> int:
-  if n == 0:
-    return 1
-  return n * factorial_recursive(n - 1)
-
-def factorial_iterative(n: int) -> int:
-  if n == 0:
-    return 1
-  
-  fact = 1
-  while n > 0:
-    fact *= n
-    n -= 1
-  
-  return fact
-
-def factorial__dp(n: int) -> int:
-  pass
-
 """ FIBONACCI """
 def fibonacci_iteretive(n: int) -> int:
   if n == 1:
