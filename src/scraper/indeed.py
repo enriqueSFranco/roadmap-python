370ea28ed1a0fd67db9fdcff0f20484738898998
enriqueSFranco <enrique.sfranco04@gmail.com>
2024-10-21 14:15:11 -0600

    bug: Problema al dar click en cada vacante, hay elementos superpuestos.

--- src/scraper/indeed.py
+++ src/scraper/indeed.py
@@ -1,6 +1,6 @@
 import asyncio
 import logging
-from playwright.async_api import async_playwright, Playwright
+from playwright.async_api import async_playwright, Locator, Page, Playwright
 
 logging.basicConfig(
   level=logging.DEBUG, 
@@ -18,11 +18,11 @@ class BrowserManager:
 
   async def start(self):
     try:
-      logger.info("Iniciando Playwright...")
+      logger.info('Iniciando Playwright...')
       chromium = self.playwright.chromium
       self._browser = await chromium.launch(headless=False, slow_mo=1000)
       self._context = await self._browser.new_context()
-      logger.info("Navegador y contexto creados.")
+      logger.info('Navegador y contexto creados.')
     except Exception as exc:
       logger.error(f'Error al iniciar el navegador: {exc}')
 
@@ -45,8 +45,44 @@ class BrowserManager:
 
   async def new_page(self):
     if not self._context:
-      raise RuntimeError("El contexto del navegador no está inicializado.")
+      raise RuntimeError('El contexto del navegador no está inicializado.')
     return await self._context.new_page()
+
+# Excepcion personalizada
+class MissingElementException(Exception):
+  def __init__(self, message, code):
+    super().__init__(message)
+    self.code = code
+
+class ElementVisibilityException(Exception):
+  def __init__(self, *args):
+    super().__init__(*args)
+
+async def fill_input_field(input: Locator, text: str):
+  if await input.count() == 0:
+    raise MissingElementException('El elemento no se encontró en el DOM.', code=404)
+  if not await input.is_visible():
+    raise MissingElementException('El campo no está visible y no se pudo llenar.', code=403)
+  try:
+    await input.fill(text)
+    logger.info(f'El campo ha sido llenado con la información: {text}')
+  except Exception as exc:
+    logger.error(f'Error al intentar llenar el campo: {exc}')
+
+class IndeedScraper:
+  def __init__(self, url):
+    self._url = url
+
+  async def fill_input_field(input: Locator, text: str):
+    if await input.count() == 0:
+      raise MissingElementException('El elemento no se encontró en el DOM.', code=404)
+    if not await input.is_visible():
+      raise MissingElementException('El campo no está visible y no se pudo llenar.', code=403)
+    try:
+      await input.fill(text)
+      logger.info(f'El campo ha sido llenado con la información: {text}')
+    except Exception as exc:
+      logger.error(f'Error al intentar llenar el campo: {exc}')
 
 async def main():
   async with async_playwright() as playwright:
@@ -58,8 +94,6 @@ async def main():
       await page.goto(URL)
       await page.wait_for_selector('form#jobsearch')
 
-      print(await page.title())
-      
       # localizar los inputs dentro del formulario para introducir la informacion
       input_what = page.locator('input#text-input-what')
       input_where = page.locator('input#text-input-where')
@@ -67,15 +101,38 @@ async def main():
 
       if input_what and input_where and btn_search:
         logger.info('✅ Inputs localizados exitosamente')
-        await input_what.fill("Desarrollador de software")
-        await input_where.fill("Ciudad de México")
+        # TODO: Manejar los errores para cada input
+        await fill_input_field(input_what, 'Desarrollador de software')
+        await fill_input_field(input_where, 'Ciudad de México')
         logger.info('✅ Inputs rellenados exitosamente')
 
         await btn_search.click()
         logger.info('✅ Botón "Buscar empleos" clicado exitosamente')
 
         # https://mx.indeed.com/jobs?q=react+junior&l=Ciudad+de+M%C3%A9xico&from=searchOnHP&vjk=5cd5ab50cff4e541
-        await page.wait_for_url("https://mx.indeed.com/jobs?q=*")
+        await page.wait_for_url('https://mx.indeed.com/jobs?q=*', timeout=2000)
+
+        # recuperar el elemento que contiene la lista de empleos
+        mosaic_jobs_results = page.locator('div[id="mosaic-jobResults"]')
+        mosaic_provider_job_cards = mosaic_jobs_results.locator('div[id="mosaic-provider-jobcards"]')
+        job_cards = mosaic_provider_job_cards.locator('ul>li')
+
+        titles = []
+        for i in range(await job_cards.count()):
+          if await job_cards.nth(i).is_visible():
+            try:
+              job_link = job_cards.nth(i).locator('a')
+              await job_cards.nth(i).click()
+              if await job_link.get_attribute('aria-pressed'):
+                await page.wait_for_load_state('networkidle')
+                
+                title_element = page.locator('h2.jobsearch-JobInfoHeader-title > span')
+                await page.wait_for_selector('h2.jobsearch-JobInfoHeader-title > span')
+                title = await title_element.inner_text()
+                print(title)
+            except Exception as exc:
+              logger.error(f'Error: {exc}')
+          await asyncio.sleep(1)
       else:
         logger.warning('❌ Inputs no encontrados')
 
@@ -87,10 +144,3 @@ async def main():
 
 if __name__ == '__main__':
   asyncio.run(main())
-
-# class IndeedScraper:
-#   def __init__(self, url):
-#     self.url = url
-  
-#   async def navigate(self, context):
-#     pass
