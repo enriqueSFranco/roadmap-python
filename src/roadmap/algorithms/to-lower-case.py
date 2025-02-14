367ce658f360e6fc92f508c71d9c0d520a9cf7d2
enriqueSFranco <enrique.sfranco04@gmail.com>
2025-02-14 10:32:22 -0600

    chore: delete files

--- src/roadmap/algorithms/to-lower-case.py
+++
@@ -1,9 +0,0 @@
-def toLowerCase(s: str) -> str:
-  result = ""
-  for char in s:
-    if "A" <= char <= "Z":
-      result += char(ord(char) + 32)
-    else: result += char
-  return result
-
-print(toLowerCase(""))
